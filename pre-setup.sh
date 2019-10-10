f564eed9f554f4af93521c818f5651100d372291
nrxr <nrxr@disroot.org>
2019-10-10 17:25:43 -0400

    pre-setup: simplify uname's and extend installer
    The checker for the OS is simpler, the usage of `uname -o | grep -q -i
    linux` catches a lot more possibilities than plain Linux. For example,
    in voidlinux with glibc it is "GNU/Linux" which wouldn't be matched with
    the former `[ "$(uname -o)" = "Linux" ]`.
    
    The installer of `rcm` is extended and allows the possibility of
    installing on linux with apk and apt if any of those commands exists. It
    won't default to xbps if the command does not exist. If it doesn't knows
    what to do then it'll fail asking to please install rcm pointing to its
    GitHub repository.

--- pre-setup.sh
+++ pre-setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-if [ "$(uname -o)" = "Darwin" ]; then
+if uname -o | grep -q "Darwin"; then
   OS='mac'
-elif [ "$(uname -o)" = "Linux" ]; then
+elif uname -o | grep -q -i linux; then
   OS='linux'
 fi
 
@@ -17,17 +17,33 @@ isinstalled=$(command -v rcup)
 
 if [ "${OS}" = "mac" ]; then
   if [ "$isinstalled" = "rcup not found" ]; then
-    printf "will install rcm with brew...\n"
-    {
-      brew tap thoughtbot/formulae
-      brew install rcm
-    }
+    if command -v brew; then
+      printf "will install rcm with brew...\n"
+      {
+        brew tap thoughtbot/formulae
+        brew install rcm
+      }
+    else
+      printf "please check in https://github.com/thoughtbot/rcm how to install rcm in your system\n"
+      exit
+    fi
   fi
   runrcup
 elif [ "${OS}" = "linux" ]; then
   if [ "$isinstalled" = "rcup not found" ]; then
-    printf "will install rcm with xbps...\n"
-    sudo xbps-install -S rcm
+    if command -v xbps-install; then
+      printf "will install rcm with xbps...\n"
+      sudo xbps-install -S rcm
+    elif command -v apk; then
+      printf "will install rcm with apk...\n"
+      sudo apk add rcm
+    elif command -v apt; then
+      printf "will install rcm with xbps...\n"
+      sudo apt install rcm
+    else
+      printf "please check in https://github.com/thoughtbot/rcm how to install rcm in your system\n"
+      exit
+    fi
   fi
   runrcup
 else

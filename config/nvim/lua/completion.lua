1990c0493232afe60767868fc6ea9bf23fb5f9f4
nrxr <github@nrxr.org>
2023-04-28 21:22:02 -0300

    chore: cleanup non-used engines

--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -6,9 +6,6 @@ cmp.setup({
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
       vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
     end,
   },
   window = {
@@ -25,9 +22,6 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
   }, {
     { name = 'buffer' },
   })
@@ -59,4 +53,3 @@ cmp.setup.cmdline(':', {
     { name = 'cmdline' }
   })
 })
-

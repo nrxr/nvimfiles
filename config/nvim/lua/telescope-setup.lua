6fec66da816f354d7f40f5c662d9bd646a9e0860
nrxr <github@nrxr.org>
2023-08-08 18:20:09 -0300

    feat(telescope): add keymaps

--- config/nvim/lua/telescope-setup.lua
+++ config/nvim/lua/telescope-setup.lua
@@ -14,3 +14,9 @@ require('telescope').setup{
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 require('telescope').load_extension('fzf')
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})

84742824d3bafab6fe5513cc70626dd09639d498
nrxr <git@nrxr.org>
2024-04-24 11:33:48 -0300

    markdown: reformat fenced languages
    Group by language type.

--- config/nvim/lua/markdown-setup.lua
+++ config/nvim/lua/markdown-setup.lua
@@ -1,3 +1,9 @@
 vim.g.gfm_syntax_enable_always = 0
 vim.g.gfm_syntax_enable_filetypes = { 'markdown' }
-vim.g.markdown_fenced_languages = { 'json', 'go', 'golang=go', 'fish', 'sh', 'console=bash', 'rust', 'lua', 'yaml', 'terraform' }
+vim.g.markdown_fenced_languages = {
+  'json', 'javascript', 'typescript',
+  'go', 'golang=go',
+  'rust',
+  'fish', 'sh', 'bash', 'lua',
+  'yaml', 'terraform',
+}

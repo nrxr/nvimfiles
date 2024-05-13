4a13576f9e4926f74126ab54c5ac74ce32dcf1ab
nrxr <git@nrxr.org>
2024-05-13 11:08:56 -0300

    telescope: add .github to find_file search_dirs
    Add the .github directory explicitly for telescope's find_file picker on
    the search_dirs option.
    
    This allows for GitHub Action files to be found when using the picker
    while still showing the files found in the current buffer, so working
      from a repository's root directory is possible.
    
    Signed-off-by: nrxr <git@nrxr.org>

--- config/nvim/lua/telescope-setup.lua
+++ config/nvim/lua/telescope-setup.lua
@@ -1,6 +1,14 @@
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
 require('telescope').setup{
+  pickers = {
+    find_files = {
+      search_dirs = { -- ensuring .github is being used
+        require('telescope.utils').buffer_dir(), -- default, buffer
+        '.github', -- add .github
+      },
+    }
+  },
   extensions = {
     fzf = {
       fuzzy = true,                    -- false will only do exact matching

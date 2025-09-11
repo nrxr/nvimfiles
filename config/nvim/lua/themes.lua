7eb46a210832a98d7b5b1b1b6c38ffefcde70555
nrxr <git@nrxr.org>
2025-09-11 14:36:38 -0300

    feat: migrate theme to tokyonight
    From catppuccin to tokyonight, love the light theme they have with day.

--- config/nvim/lua/themes.lua
+++ config/nvim/lua/themes.lua
@@ -5,23 +5,23 @@ require("catppuccin").setup({
         light = "latte",
         dark = "frappe",
     },
-    transparent_background = false, -- disables setting the background color.
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+    transparent_background = true, -- disables setting the background color.
+    show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
     term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
         enabled = false, -- dims the background color of inactive window
         shade = "dark",
         percentage = 0.15, -- percentage of the shade to apply to the inactive window
     },
-    no_italic = false, -- Force no italic
+    no_italic = true, -- Force no italic
     no_bold = false, -- Force no bold
     no_underline = false, -- Force no underline
     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
         comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
-        loops = { "italic" },
-        functions = { "italic" },
-        keywords = { "italic" },
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
         strings = {},
         variables = {},
         numbers = {},
@@ -49,4 +49,27 @@ require("catppuccin").setup({
 })
 
 -- setup must be called before loading
-vim.cmd.colorscheme "catppuccin"
+-- vim.cmd.colorscheme "catppuccin"
+
+require("tokyonight").setup({
+  style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = false, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = false },
+    keywords = { italic = false },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+})
+vim.o.background = "dark"
+vim.cmd.colorscheme "tokyonight-storm"

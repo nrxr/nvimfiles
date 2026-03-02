793a1aabf49028affa8a5346729b6c1d42f757ef
nrxr <git@nrxr.org>
2026-03-02 14:52:26 -0300

    feat: move to cyberdream theme
    Just found this very cool theme for neovim that also has themes for the
    tools I use: ghostty, fish, tmux and zellij.
    
    Uses transparent background and looks really cool overall. No longer the
    boring plain backgrounds.
    
    https://github.com/scottmckendry/cyberdream.nvim

--- config/nvim/lua/themes.lua
+++ config/nvim/lua/themes.lua
@@ -51,25 +51,82 @@
 -- setup must be called before loading
 -- vim.cmd.colorscheme "catppuccin"
 
-require("tokyonight").setup({
-  style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-  light_style = "day", -- The theme is used when the background is set to light
-  transparent = false, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-  styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = false },
-    keywords = { italic = false },
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "transparent", -- style for sidebars, see below
-    floats = "transparent", -- style for floating windows
-  },
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+-- require("tokyonight").setup({
+--   style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+--   light_style = "day", -- The theme is used when the background is set to light
+--   transparent = false, -- Enable this to disable setting the background color
+--   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+--   styles = {
+--     -- Style to be applied to different syntax groups
+--     -- Value is any valid attr-list value for `:help nvim_set_hl`
+--     comments = { italic = false },
+--     keywords = { italic = false },
+--     functions = {},
+--     variables = {},
+--     -- Background styles. Can be "dark", "transparent" or "normal"
+--     sidebars = "transparent", -- style for sidebars, see below
+--     floats = "transparent", -- style for floating windows
+--   },
+--   day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+--   dim_inactive = false, -- dims inactive windows
+--   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+-- })
+-- vim.o.background = "dark"
+-- vim.cmd.colorscheme "tokyonight"
+
+require("cyberdream").setup({
+    -- Set light or dark variant
+    variant = "default", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+
+    -- Enable transparent background
+    transparent = true,
+
+    -- Reduce the overall saturation of colours for a more muted look
+    saturation = 1, -- accepts a value between 0 and 1. 0 will be fully desaturated (greyscale) and 1 will be the full color (default)
+
+    -- Enable italics comments
+    italic_comments = false,
+
+    -- Replace all fillchars with ' ' for the ultimate clean look
+    hide_fillchars = false,
+
+    -- Apply a modern borderless look to pickers like Telescope, Snacks Picker & Fzf-Lua
+    borderless_pickers = false,
+
+    -- Set terminal colors used in `:terminal`
+    terminal_colors = true,
+
+    -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+    cache = false,
+
+    -- Override highlight groups with your own colour values
+    highlights = {
+        -- Highlight groups to override, adding new groups is also possible
+        -- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
+
+        -- Example:
+        -- Comment = { fg = "#696969", bg = "NONE", italic = true },
+
+        -- More examples can be found in `lua/cyberdream/extensions/*.lua`
+    },
+       -- Disable or enable colorscheme extensions
+    extensions = {
+        telescope = true,
+        notify = true,
+        mini = true,
+    },
+
+    -- Alternatively, you can use 'default' to set all extensions at once
+    -- cache = true, -- Use cache for fastest loads
+    -- extensions = {
+    --     default = false, -- Disable all by default
+    --     base = true, -- Enable all built-in hl groups (you probably want this)
+    --
+    --     -- Now enable only what you want to use
+    --     telescope = true,
+    --     cmp = true,
+    --     gitsigns = true,
+    -- },
 })
-vim.o.background = "dark"
-vim.cmd.colorscheme "tokyonight"
+
+vim.cmd.colorscheme "cyberdream"
